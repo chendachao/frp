f94709632dfbe874195f941a124cfa6180073a8f
chendachao <chendachao@outlook.com>
2019-08-17 22:36:12 +0800

    s

--- manifest-tool.sh
+++ manifest-tool.sh
@@ -3,8 +3,8 @@
 
 # frpc
 manifest-tool push from-spec frpc.yaml
-# manifest-tool push from-spec frpc-latest.yaml
+manifest-tool push from-spec frpc-latest.yaml
 
 # frps
 manifest-tool push from-spec frps.yaml
-# manifest-tool push from-spec frps-latest.yaml
+manifest-tool push from-spec frps-latest.yaml
