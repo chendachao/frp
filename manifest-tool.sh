0fff154ff690f5c02696c23c256a4e49085929a2
chendachao <chendachao@outlook.com>
2019-08-17 18:00:51 +0800

    refactor

--- manifest-tool.sh
+++ manifest-tool.sh
@@ -2,9 +2,9 @@
 # https://github.com/estesp/manifest-tool
 
 # frpc
-manifest-tool push from-spec frpc.yaml
-manifest-tool push from-spec frpc-latest.yaml
+# manifest-tool push from-spec frpc.yaml
+# manifest-tool push from-spec frpc-latest.yaml
 
 # frps
-manifest-tool push from-spec frps.yaml
+# manifest-tool push from-spec frps.yaml
 manifest-tool push from-spec frps-latest.yaml
